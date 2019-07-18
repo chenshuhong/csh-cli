607316e30b5324e1beb88eebcd8acb01525e4cd1
baiye <baiye.csh@dtyunxi.com>
2019-07-18 18:05:09 +0800

    模板更新

---
+++ genTest/data.js
@@ -0,0 +1,17 @@
+/**
+ * @Author: 陈树鸿
+ * @Date: 2019-07-18 17:55
+ */
+module.exports = {
+  serv:{
+    list:{
+      url:'baiye/list'
+    },
+    changeStatus:{
+      url:'baiye/changeStatus'
+    },
+    del:{
+      url:'baiye/delete'
+    }
+  }
+}
