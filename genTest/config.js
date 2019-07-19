0d11db2499c09b22be605f9780daacc70b0bf129
baiye <baiye.csh@dtyunxi.com>
2019-07-19 15:21:54 +0800

    模板更新之view界面

--- genTest/config.js
+++ genTest/config.js
@@ -13,5 +13,42 @@ module.exports = {
     del:{
       url:'baiye/delete'
     }
+  },
+  view:{
+    queryConfig:[
+      {
+        zh_name: '权益包名称',
+        en_name: 'rightPackageName',
+      },
+      {
+        elem_type: 'Input',
+        zh_name: '店铺范围',
+        en_name: 'area',
+      },
+    ],
+    columns:[
+      { title: '权益包编号', dataIndex: 'rightPackageCode' },
+      { title: '权益包名称', dataIndex: 'rightPackageName' },
+      { title: '包含权益数量', dataIndex: 'num' },
+      { title: '最大库存', dataIndex: 'maxInventory' },
+      { title: '已发放库存', dataIndex: 'outInventory' },
+      { title: '生效时间', dataIndex: 'startTime' },
+      { title: '失效时间', dataIndex: 'endTime' },
+      { title: '状态', dataIndex: 'status' },
+      { title: '操作', dataIndex: 'operation', width: 200, fixed: 'right'}
+    ],
+    operationBtns:[
+      {label:'查看'},
+      {label:'屏蔽'},
+      {label:'编辑'},
+      {label:'删除',isConfirm:true,confirmTitle:'是否确定删除'},
+    ],
+    showRowSelection:true,
+    gridBtn:[
+      {label:'新增权益包'},
+      {label:'批量展示'},
+      {label:'批量屏蔽'},
+      {label:'批量删除'},
+    ]
   }
 }
--- src/createTemplate.js
+++ src/createTemplate.js
@@ -19,22 +19,36 @@ if (commander.template){
   const viewPath = path.resolve(currentPath, `${templateName}View.js`);
   const modPath = path.resolve(currentPath, `${templateName}Mod.js`);
   const configPath = path.resolve(currentPath, `config.js`);
-  let {serv} = require(configPath)
+  let {serv,view} = require(configPath)
   let log = console.log
   try{
+    //复制style.less
     log(chalk.green('copy style.less start'))
     fs.copyFileSync('../template/style.less',stylePath)
-    log(chalk.green('copy style.less success'))
+    log(chalk.green(`copy style.less to ${templateName}Style.less success`))
+    
+    //复制及修改serv.js文件
     chalk.green('read serv.js start')
-    let servEjs = fs.readFileSync('../template/serv.ejs').toString()
-    log(chalk.green('read serv.js success,write *Serv.js start'))
+    let servFileString = fs.readFileSync('../template/serv.js').toString()
+    log(chalk.green(`read serv.js success,write ${templateName}Serv.js start`))
     const servData = {
       listUrl:serv.list.url,
       changeStatusUrl:serv.changeStatus.url,
       deleteUrl:serv.del.url
     }
-    fs.writeFileSync(servPath,ejs.render(servEjs,servData))
-    log(chalk.green('write *Serv.js success'))
+    fs.writeFileSync(servPath,ejs.render(servFileString,servData))
+    log(chalk.green(`write ${templateName}Serv.js success`))
+  
+    //复制及修改view.js文件
+    chalk.green('read view.js start')
+    let viewFileString = fs.readFileSync('../template/view.ejs').toString()
+    log(chalk.green(`read view.js success,write ${templateName}View.js start`))
+    const viewData = {
+      name:templateName,
+      ...view
+    }
+    fs.writeFileSync(viewPath,ejs.render(viewFileString,viewData))
+    log(chalk.green(`write ${templateName}View.js success`))
   }catch (e) {
     log(chalk.red('error occur',e))
   }
